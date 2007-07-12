a33059e657a4ca756e231f2914271716224eb04e
harkins <harkins>
2007-07-12 22:54:57 +0000

    r818: Fixed bug in error-handling; better console log.

--- workers/fetcher.rb
+++ workers/fetcher.rb
@@ -11,8 +11,8 @@ Net::POP3.delete_all(MAIL_SERVER, MAIL_POP3_PORT, MAIL_USER, MAIL_PASSWORD) do |
   begin
     message.store
   rescue Exception => e
-    puts "#{mail.number} failed to store"
-    AWS::S3::S3Object.store(message.generated_id, e.message + "\n" + e.backtrace + "\n\n" + message.message, 'listlibrary_storage_failed')
+    puts "#{mail.number} failed to store: #{e.message}; failure stored as #{message.generated_id}"
+    AWS::S3::S3Object.store(message.generated_id, e.message + "\n" + e.backtrace.join("\n") + "\n\n" + message.message, 'listlibrary_storage_failed', :content_type => "text/plain" )
     next
   end
   puts "#{mail.number} #{message.bucket} #{message.filename}"
