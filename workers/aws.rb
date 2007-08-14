641c4e49267d9cd01c9f14b225e822273dfee5b2
harkins <harkins>
2007-08-14 02:43:28 +0000

    r871: Easy way to get a list of keys of unlimited length.

--- workers/aws.rb
+++ workers/aws.rb
@@ -8,6 +8,24 @@ require 'filer'
 ACCESS_KEY_ID = '0B8FSQ35925T27X8Q4R2'
 SECRET_ACCESS_KEY = 'ryM3xNKV/3OL9j5jMeJHRqSzWETxK5MeSlXj6/rv'
 
+class AWS::S3::Bucket
+  def self.keylist bucket, prefix
+    last = prefix
+    keys = []
+    loop do
+      list = begin
+        get(path(bucket, { :prefix => prefix, :marker => last})).parsed['contents'].collect { |o| o['key'] }
+      rescue NoMethodError
+        []
+      end
+      break if list.empty?
+      keys += list
+      last = keys.last
+    end
+    keys
+  end
+end
+
 unless defined? AWS_connection
   AWS_connection = AWS::S3::Base.establish_connection!(
     :access_key_id     => ACCESS_KEY_ID,
