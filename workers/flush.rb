781991f69ccc961631af86dfabc004b7943a5721
harkins <harkins>
2007-08-31 19:24:21 +0000

    r951: Flush script.

---
+++ workers/flush.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/ruby
+
+require 'aws'
+
+if ARGV.empty?
+  puts "call with one or more slug[/year[/month]]"
+  exit
+end
+
+# build list of months to flush
+jobs = []
+ARGV.each do |url|
+  slug, year, month = url.split('/')
+
+  if year and month
+    jobs << { :slug => slug, :year => year, :month => month }
+  else
+    prefix = "inventory/#{slug}/"
+    prefix += "#{year}/" if year
+    AWS::S3::Bucket.keylist('listlibrary_cachedhash', prefix).each do |key|
+      y, m= key.split('/')[2..-1]
+      jobs << { :slug => slug, :year => y, :month => m }
+    end
+  end
+end
+
+thread_queue = CachedHash.new 'thread_queue'
+
+jobs.each do |job|
+  slug, year, month = job[:slug], job[:year], job[:month]
+
+  # delete threading job
+  AWS::S3::S3Object.delete("thread_queue/#{slug}/#{year}/#{month}", 'listlibrary_cachedhash')
+
+  # delete render jobs
+  AWS::S3::Bucket.keylist('listlibrary_cachedhash', "render_queue/#{slug}/#{year}/#{month}") do |key|
+    AWS::S3::S3Object.delete(key, 'listlibrary_cachedhash')
+  end
+
+  # delete message cache
+  AWS::S3::S3Object.delete("list/#{slug}/message_cache/#{year}/#{month}", 'listlibrary_archive')
+
+  # delete thread cache
+  AWS::S3::Bucket.keylist('listlibrary_archive', "list/#{slug}/thread/#{year}/#{month}") do |key|
+    AWS::S3::S3Object.delete(key, 'listlibrary_archive')
+  end
+
+  # delete inventory
+  AWS::S3::S3Object.delete("inventory/#{slug}/#{year}/#{month}", 'listlibrary_cachedhash')
+
+  # queue rethread
+  thread_queue["#{slug}/#{year}/#{month}"] = ''
+end
