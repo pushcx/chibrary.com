f179d5c68221ee085e2d38cbd70f5cf4a4e50e98
harkins <harkins>
2008-01-02 17:18:21 +0000

    r1194: Call date properly BugzID: 196

--- lib/message.rb
+++ lib/message.rb
@@ -128,7 +128,7 @@ class Message
   end
 
   def load_key
-    @key = "list/#{@slug}/message/#{date.year}/%02d/#{@message_id}" % @date.month
+    @key = "list/#{@slug}/message/#{@date.year}/%02d/#{@message_id}" % [@date.month]
   end
 
   def load_from
