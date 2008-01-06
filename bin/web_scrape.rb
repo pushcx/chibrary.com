cb88f95c3ca9e413c84e3f3a8deb6e85eecbf462
harkins <harkins>
2008-01-06 21:00:47 +0000

    r1216: Updated scraper to grab messages in parallel.

--- bin/web_scrape.rb
+++ bin/web_scrape.rb
@@ -7,18 +7,23 @@ CWD = Dir.getwd
 
 {
 #  'ruby-math' => 1026,
-  'ruby-ext'  => 2324,
-  'ruby-core' => 14083,
+#  'ruby-ext'  => 2324,
+#  'ruby-core' => 14083,
   'ruby-list' => 44444,
-  'ruby-dev'  => 32932,
-  'ruby-talk' => 286201,
+#  'ruby-dev'  => 32932,
+  'ruby-talk' => 286201, #
 }.each do |list, count|
   puts '*' * 80, list
   Dir.chdir "scraped/#{list}/new"
   for i in (1..count)
-    puts "#{list} #{1.0 * i/count * 100}" if i % 100 == 0
-    `wget http://blade.nagaokaut.ac.jp/ruby/#{list}/#{i} -t 5 -q -c -N --waitretry=30`
+    fork do
+      exec("wget http://blade.nagaokaut.ac.jp/ruby/#{list}/#{i} -t 5 -q -c -N --waitretry=30")
+    end
+    if i % 40 == 0
+      puts "#{list} #{1.0 * i/count * 100}"
+      Process.waitall
+      sleep 1
+    end
   end
   Dir.chdir CWD
-  exit
 end
