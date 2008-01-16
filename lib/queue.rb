a63b8f89048a03757125c03d75ab7071386de920
harkins <harkins>
2008-01-16 01:32:36 +0000

    r1268: Start of centralized queue and job system. BugzID: 208

---
+++ lib/queue.rb
@@ -0,0 +1,56 @@
+require 'rubygems'
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'aws'
+
+JOB_TYPES = {
+  :thread => {
+    :key => ":slug/:year/:month",
+  },
+  :render_static => {
+    :key => '',
+  },
+  :render_list => {
+    :key => ":slug",
+  },
+  :render_month => {
+    :key => ":slug/:year/:month",
+  },
+  :render_thread => {
+    :key => ":slug/:year/:month/:call_number",
+  },
+}
+
+class Job
+  attr_accessor :type, :attributes
+
+  def initialize type, attributes
+    raise "unknown job type #{type}" unless JOB_TYPES.has_key? type
+    @type = type
+    @attributes = attributes
+  end
+
+  def key
+    "#{@type.to_s}/" + JOB_TYPES[@type][:key].gsub(/:(\w+)/) { @attributes[$1.to_sym] }
+  end
+end
+
+class Queue
+  attr_reader :type
+
+  def initialize type
+    raise "unknown job type #{type}" unless JOB_TYPES.has_key? type
+    @type = type
+    @queue = CachedHash.new("queue/#{type}")
+    @stop_on_empty = false
+  end
+
+  def add type, attributes
+    job = Job.new type, attributes
+    @queue[job.key] = job.to_yaml
+  end
+
+  def next
+    # generic worker?
+  end
+end
---
+++ test/unit/queue.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'queue'
+
+class JobTest < Test::Unit::TestCase
+  def test_new_bad_type
+    assert_raises(RuntimeError, /unknown job type/) do
+      Job.new :foo, []
+    end
+  end
+
+  def test_new
+    job = Job.new :render_list, { :slug => 'example' }
+    assert_equal :render_list, job.type
+    assert_equal 'example', job.attributes[:slug]
+  end
+
+  def test_key
+    job = Job.new :render_month, { :slug => 'example', :year => 2008, :month => '01' }
+    assert_equal 'render_month/example/2008/01', job.key
+  end
+end
