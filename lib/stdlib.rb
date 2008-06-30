734c34e0f365f85ad65ffb22b7735699c83323f2
Peter Harkins <ph@malaprop.org>
2008-06-30 07:27:39 -0400

    Guard against Rails' already-done implementation

--- lib/stdlib.rb
+++ lib/stdlib.rb
@@ -1,9 +1,11 @@
-module Enumerable
-  def sum ; inject(0) { |x, y| x + y }; end
-end
+unless defined? Rails
+  module Enumerable
+    def sum ; inject(0) { |x, y| x + y }; end
+  end
 
-class Symbol
-  def to_proc
-    proc { |obj, *args| obj.send(self, *args) }
+  class Symbol
+    def to_proc
+      proc { |obj, *args| obj.send(self, *args) }
+    end
   end
 end
