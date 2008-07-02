b23acd60196b2d01da5f3419bc1d582697e283b3
Peter Harkins <ph@malaprop.org>
2008-07-02 16:14:41 -0400

    Downgrading to DreamHost's Rails 2.0.2

--- listlibrary.net/config/environment.rb
+++ listlibrary.net/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -34,6 +34,8 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   #config.load_paths += %W( #{RAILS_ROOT}/../lib/ )
 
+  ENV['GEM_PATH'] = '/home/listlibrary/gems' if ENV['RAILS_ENV'] == 'production'
+
   require 'filer' # for the Integer patches
   require 'list'
   require 'storage'
@@ -50,7 +52,7 @@ Rails::Initializer.run do |config|
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
-  config.time_zone = 'UTC'
+  #config.time_zone = 'UTC'
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
