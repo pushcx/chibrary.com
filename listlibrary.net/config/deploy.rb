5725c89ca8c44c3b7f7cfcd29bfae59f384248b3
Peter Harkins <ph@malaprop.org>
2008-07-02 15:05:41 -0400

    Capistrano setup

---
+++ listlibrary.net/config/deploy.rb
@@ -0,0 +1,31 @@
+require 'mongrel_cluster/recipes'
+
+set :application, "listlibrary.net"
+
+set :scm, :git
+set :repository, "harkins@argyle.redirectme.net:~/biz/listlibrary"
+
+set :deploy_to, "/home/listlibrary/listlibrary.net"
+set :deploy_via, :copy
+
+set :user, "listlibrary"
+set :domain, "tron.dreamhost.com"
+set :use_sudo, false
+set :ssh_options, { :forward_agent => true }
+set :spinner_user, nil
+
+role :web, "listlibrary.net"
+role :app, "listlibrary.net"
+role :db, "listlibrary.net"
+
+namespace :deploy do
+  desc "Restarting Passenger with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "Task '#{t}' is unneeded with Passenger"
+    task t, :roles => :app do ; end
+  end
+end
