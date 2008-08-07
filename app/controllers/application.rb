acd02414680777a6f1a167fb0f84090b6aab6192
Peter Harkins <ph@malaprop.org>
2008-08-07 13:07:45 -0400

    Added exception_notification plugin

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include ExceptionNotifiable
   session :off
   helper :all # include all helpers, all the time
   helper_method :f, :subject
--- config/environment.rb
+++ config/environment.rb
@@ -68,3 +68,7 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
+
+ExceptionNotifier.exception_recipients = %w(harkins@barkingstapler.com)
+ExceptionNotifier.sender_address = "postmaster@listlibrary.net"
+ExceptionNotifier.email_prefix = ''
