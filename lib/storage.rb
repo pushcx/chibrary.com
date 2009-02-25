pushcx/chibrary.com

commit 4209ecdb7ffb3d9a0e8a958bc6cc812d966a62fd

Got all storage tests passing

lib/storage.rb
@@ -97,11 +97,12 @@ class ZDir
   def each(recurse=false)
     Dir.entries(@path).sort.each do |path|
       next if %w{. ..}.include? path
-      yield path
       if File.directory? File.join(@path, path)
         ZDir.new([@path, path].join('/')).each(recurse) { |p| yield File.join(path, p) } if recurse
       elsif path =~ /\.zip$/
         ZZip.new([@path, path].join('/')).each(recurse) { |p| yield File.join(path, p) } if recurse
+      else
+        yield path
       end
     end
   end

test/unit/storage.rb
@@ -134,7 +134,7 @@ def test_truth ; assert true ; end
 
   def test_first
     z = ZDir.new("/tmp/test")
-    assert_match /mail.@example\.com/, z.first
+    assert_match /foo\/nested@example\.com/, z.first
   end
 
   def test_lookup

commit acc15ed50ea36f333c7791f82ca9fdec0fa5dbed

Fixed bugs introduced in 4209ecd - dirs should be yielded.
This kept the homepage from displaying lists.

lib/storage.rb
@@ -97,12 +97,11 @@ class ZDir
   def each(recurse=false)
     Dir.entries(@path).sort.each do |path|
       next if %w{. ..}.include? path
+      yield path
       if File.directory? File.join(@path, path)
         ZDir.new([@path, path].join('/')).each(recurse) { |p| yield File.join(path, p) } if recurse
       elsif path =~ /\.zip$/
         ZZip.new([@path, path].join('/')).each(recurse) { |p| yield File.join(path, p) } if recurse
-      else
-        yield path
       end
     end
   end

test/unit/storage.rb
@@ -133,14 +133,14 @@ def test_truth ; assert true ; end
 
   def test_each
     z = ZDir.new("/tmp/test")
-    assert_equal ['mail1@example.com', 'mail2@example.com'], z.collect.sort
+    assert_equal ['foo', 'mail1@example.com', 'mail2@example.com'], z.collect.sort
   end
 
   def test_each_recurse
     z = ZDir.new("/tmp/test")
     l = []
     z.each(true) {|f| l << f }
-    assert_equal ['foo/nested@example.com', 'mail1@example.com', 'mail2@example.com'], l.sort
+    assert_equal ['foo', 'foo/nested@example.com', 'mail1@example.com', 'mail2@example.com'], l.sort
   end
 
   def test_first
@@ -171,7 +171,7 @@ def test_truth ; assert true ; end
   def test_delete
     z = ZDir.new("/tmp/test")
     z.delete('mail1@example.com')
-    assert_equal ['mail2@example.com'], z.collect.sort
+    assert_equal ['foo', 'mail2@example.com'], z.collect.sort
   end
   
   def test_delete_path