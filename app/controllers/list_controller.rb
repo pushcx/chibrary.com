60aa62102374ba5b44969d37b8a01542c16616ea
Peter Harkins <ph@malaprop.org>
2009-02-24 22:52:34 -0600

    Redirect years to list pages

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,9 +1,13 @@
 class ListController < ApplicationController
-  before_filter :load_list, :load_list_snippets
+  before_filter :load_list, :load_list_snippets, :except => :year_redirect
   caches_page :show
 
   def show
     @title = "#{@list['name'] or @slug} archive"
     @year_counts = ThreadList.year_counts @slug
+  end
+
+  def year_redirect
+    redirect_to :action => 'show'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,8 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '',       :controller => 'generic', :action => 'homepage'
   map.connect 'search', :controller => 'generic', :action => 'search'
   
-  map.connect ':slug/',                          :controller => 'list',   :action => 'show'
+  map.connect ':slug',                           :controller => 'list',   :action => 'show'
+  map.connect ':slug/:year',                     :controller => 'list',   :action => 'year_redirect'
   map.connect ':slug/:year/:month',              :controller => 'month',  :action => 'show'
   map.connect ':slug/:year/:month/:call_number', :controller => 'thread', :action => 'show'
 end
