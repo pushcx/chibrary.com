7e664024f0a0463d7a55300cf18c4a9cc4e31aed
Peter Harkins <ph@malaprop.org>
2009-12-01 18:53:04 -0600

    Ran 'rails .' with Rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -26,6 +26,13 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem 'haml'
+
+  # testing
+  config.gem 'mocha'
+  config.gem 'redgreen'
+
+  # production monitoring
   config.gem 'json'
   config.gem 'fiveruns-dash-rails',
              :lib => 'fiveruns_dash_rails',
@@ -46,6 +53,7 @@ Rails::Initializer.run do |config|
     next unless lib =~ /.rb$/
     require lib
   end
+  require 'pp'
   require 'threading'
 
   # Force all environments to use the same logger level
@@ -76,4 +84,8 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+
+  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+  # config.i18n.default_locale = :de
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,46 +1,50 @@
-require 'rubygems'
-require 'test/unit'
-require 'haml'
-require 'mocha'
-require 'pp'
-require 'redgreen'
-require 'yaml'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'test_help'
 
-require File.dirname(__FILE__) + '/../config/boot'
-require "#{RAILS_ROOT}/config/environment"
+class ActiveSupport::TestCase
+  # Transactional fixtures accelerate your tests by wrapping each test method
+  # in a transaction that's rolled back on completion.  This ensures that the
+  # test database remains unchanged so your fixtures don't have to be reloaded
+  # between every test method.  Fewer database queries means faster tests.
+  #
+  # Read Mike Clark's excellent walkthrough at
+  #   http://clarkware.com/cgi/blosxom/2005/10/24#Rails10FastTesting
+  #
+  # Every Active Record database supports transactions except MyISAM tables
+  # in MySQL.  Turn off transactional fixtures in this case; however, if you
+  # don't care one way or the other, switching from MyISAM to InnoDB tables
+  # is recommended.
+  #
+  # The only drawback to using transactional fixtures is when you actually 
+  # need to test transactions.  Since your test is bracketed by a transaction,
+  # any transactions started in your code will be automatically rolled back.
+  self.use_transactional_fixtures = true
 
-# path magic from p152 of Programming Ruby
-$:.unshift File.join(File.dirname(__FILE__), "..", "script")
-#$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+  # Instantiated fixtures are slow, but give you @david where otherwise you
+  # would need people(:david).  If you don't want to migrate your existing
+  # test cases which use the @david style and don't mind the speed hit (each
+  # instantiated fixtures translates to a database query per test method),
+  # then set this back to true.
+  self.use_instantiated_fixtures  = false
 
-require 'storage'
-$archive = $cachedhash = nil # avoid accidental real changes
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
 
-# Don't ever actually make pop connections
-require 'net/pop'
-class Net::POP3
-  def initialize server, port ; raise "un-mocked POP3 call" ; end
-end
+  $archive = $cachedhash = nil # avoid accidental real changes
 
-# Don't really log dev stuff
-require 'log'
-class Log
-  def in_test_mode? ; true ; end
-  def log status, message ; message ; end
-end
-
-class Test::Unit::TestCase
-  @@fixtures = {}
-  def self.fixtures list
-    [list].flatten.each do |fixture|
-      self.class_eval do
-        define_method(fixture) do |item|
-          filename = File.join(File.dirname(__FILE__), 'fixtures', "#{fixture.to_s}.yaml")
-          @@fixtures[fixture] ||= YAML::load_file(filename)
-          @@fixtures[fixture][item.to_s]
-        end
-      end
-    end
+  require 'net/pop'
+  class Net::POP3
+    def initialize server, port ; raise "un-mocked POP3 call" ; end
   end
-end
 
+  require 'log'
+  class Log
+    def in_test_mode? ; true ; end
+    def log status, message ; message ; end
+  end
+
+end
