23b7120a20574fc9aaa218a60d7bc8c5b3ce33ed
Peter Harkins <ph@malaprop.org>
2009-12-03 20:53:45 -0600

    Added more GenericControllerTests

--- test/functional/generic_controller_test.rb
+++ test/functional/generic_controller_test.rb
@@ -2,9 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class GenericControllerTest < ActionController::TestCase
   context "on GET to /about" do
-    setup do
-      get :about
-    end
+    setup { get :about }
 
     should_respond_with :success
     should_render_template :about
@@ -12,12 +10,23 @@ class GenericControllerTest < ActionController::TestCase
   end
 
   context "on GET to /search" do
+    setup { get :search }
+
+    should_respond_with :success
+    should_render_template :search
+    should_not_set_the_flash
+  end
+
+  context "on GET to /" do
     setup do
-      get :search
+      $archive.expects(:[]).with('list').returns([])
+      $archive.expects(:[]).with('snippet/homepage').returns([])
+      get :homepage
     end
 
     should_respond_with :success
-    should_render_template :about
+    should_assign_to :lists, :snippets
+    should_render_template :homepage
     should_not_set_the_flash
   end
 end
