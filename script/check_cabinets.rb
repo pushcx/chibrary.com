20ce4546536f78180a14b6c0781bed27d72f4bda
Peter Harkins <ph@malaprop.org>
2010-04-22 18:20:07 -0500

    Script to confirm cabinet migration succeeded

---
+++ script/check_cabinets.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__) + '/../config/boot'
+require "#{RAILS_ROOT}/config/environment"
+
+Dir['listlibrary_archive/list/*'].each do |list_path|
+  puts "#{Time.now} #{list_path}"
+  list_path = list_path[0..-5] # strip off .tcb
+  slug = list_path.split('/').last
+
+  count = 0
+  $archive["list/#{slug}"].each do |path|
+    cab_file = $archive["list/#{slug}/#{path}"]
+    old_file = $archive["old_list/#{slug}/#{path}"]
+    raise "crap (at #{count}) - #{slug}/#{path} #{cab_file.class} #{old_file.class}\n#{cab_file.to_yaml}\n#{old_file.to_yaml}" if cab_file != old_file
+    count += 1
+  end
+  puts "#{count} messages checked"
+
+end
+puts Time.now
