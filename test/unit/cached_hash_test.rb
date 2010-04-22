c492c7fb48630c41df27d01c8706ae06629474da
Peter Harkins <ph@malaprop.org>
2010-04-22 18:48:50 -0500

    Converted CachedHash tests to shoulda.
    BugzId: 1116

--- test/unit/cached_hash_test.rb
+++ test/unit/cached_hash_test.rb
@@ -1,20 +1,25 @@
 require 'test_helper'
 
 class CachedHashTest < ActiveSupport::TestCase
+  context 'a cachedhash instance' do
+    setup do
+      @ch = CachedHash.new 'unit_test'
+    end
+    
+    should 'turn NotFound into nil' do
+      $archive.expects(:[]).with('unit_test/missing').raises(NotFound)
+      assert_nil @ch['missing']
+    end
 
-  def test_find
-    ch = CachedHash.new 'unit_test'
-    assert_equal CachedHash, ch.class
-    $archive.expects(:[]).with('unit_test/missing').raises(NotFound)
-    assert_nil ch['missing']
-    $archive.expects(:[]).with('unit_test/working').returns(mock(:chomp => 'working'))
-    assert_equal 'working', ch['working']
-  end
+    should 'find keys' do
+      $archive.expects(:[]).with('unit_test/working').returns(mock(:chomp => 'working'))
+      assert_equal 'working', @ch['working']
+    end
 
-  def test_store
-    ch = CachedHash.new 'unit_test'
-    $archive.expects(:[]=).with('unit_test/key', 'value')
-    ch['key'] = 'value'
-    assert_equal 'value', ch['key']
+    should 'store' do
+      $archive.expects(:[]=).with('unit_test/key', 'value')
+      @ch['key'] = 'value'
+      assert_equal 'value', @ch['key']
+    end
   end
 end
