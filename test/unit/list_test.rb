d286b6f86a1d15d9c62b71a27aa2ca5d9dbf5288
Peter Harkins <ph@malaprop.org>
2010-04-22 20:38:49 -0500

    Converted List tests to shoulda.
    BugzId: 1116

--- test/unit/list_test.rb
+++ test/unit/list_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  def setup
-    @list = List.new 'slug'
-  end
-
   should "recognize invalid slugs that are too long" do
     assert_raises InvalidSlug do
       List.new '01234567890123456789x'
@@ -17,37 +13,44 @@ class ListTest < ActiveSupport::TestCase
     end
   end
 
-  def test_slug
-    assert_equal 'slug', @list.slug
-  end
+  context 'a list' do
+    setup do
+      @list = List.new 'slug'
+    end
 
-  def test_cached_message_list_empty
-    $archive.expects(:[]).returns(nil)
-    assert_equal [], @list.cached_message_list("2008", "01")
-  end
+    should 'have a slug' do
+      assert_equal 'slug', @list.slug
+    end
 
-  def test_cached_message_list
-    $archive.expects(:[]).returns(["1@example.com", "2@example.com"])
-    assert_equal ["1@example.com", "2@example.com"], @list.cached_message_list("2008", "01")
-  end
+    should 'return empty list when cache is empty' do
+      $archive.expects(:[]).returns(nil)
+      assert_equal [], @list.cached_message_list("2008", "01")
+    end
 
-  def test_fresh_message_list_empty
-    $archive.expects(:[]).returns(mock(:collect => []))
-    assert_equal [], @list.fresh_message_list("2008", "01")
-  end
+    should 'fetch cached lists' do
+      $archive.expects(:[]).returns(["1@example.com", "2@example.com"])
+      assert_equal ["1@example.com", "2@example.com"], @list.cached_message_list("2008", "01")
+    end
 
-  def test_fresh_message_list
-    $archive.expects(:[]).returns(mock(:collect => ["1@example.com", "2@example.com"]))
-    assert_equal ["1@example.com", "2@example.com"], @list.fresh_message_list("2008", "01")
-  end
+    should 'fetch an empty fresh message lists' do
+      $archive.expects(:[]).returns(mock(:collect => []))
+      assert_equal [], @list.fresh_message_list("2008", "01")
+    end
 
-  def test_cache_message_list
-    $archive.expects(:[]=)
-    @list.cache_message_list "2008", "01", ["1@example.com", "2@example.com"]
-  end
+    should 'fetch fresh message lists' do
+      $archive.expects(:[]).returns(mock(:collect => ["1@example.com", "2@example.com"]))
+      assert_equal ["1@example.com", "2@example.com"], @list.fresh_message_list("2008", "01")
+    end
 
-  def test_thread_list
-    ThreadList.expects(:new).with('slug', '2008', '01')
-    @list.thread_list("2008", "01")
+    should 'cache message lists' do
+      $archive.expects(:[]=)
+      @list.cache_message_list "2008", "01", ["1@example.com", "2@example.com"]
+    end
+
+    should 'fetch thread lists' do
+      ThreadList.expects(:new).with('slug', '2008', '01')
+      @list.thread_list("2008", "01")
+    end
+
   end
 end
