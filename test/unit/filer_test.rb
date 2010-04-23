bbf906c47b1a0551b3ed904561e33e2e051cbd1e
Peter Harkins <ph@malaprop.org>
2010-04-22 20:24:27 -0500

    Converted Filer tests to shoulda.
    BugzId: 1116

--- test/unit/filer_test.rb
+++ test/unit/filer_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class IntegerTest < ActiveSupport::TestCase
-  def test_to_base_64
+  should 'convert integers to base 64' do
     [
       [0, '00000000'], # base case
       [1, '00000001'], # add one
@@ -28,105 +28,107 @@ end
 class FilerTest < ActiveSupport::TestCase
   fixtures :message
 
-  def setup
-    # Many tests would otherwise cause the Filer to print its normal output
-    #$stdout.expects(:puts).at_least(0)
-  end
-
-  def test_acquire_unsubclassed
+  should 'not work unless subclassed' do
     f = Filer.new(0, 0)
     assert_raises(RuntimeError, /subclass/) do
       f.acquire
     end
   end
 
-  def test_store
-    message = mock('message', :store => true, :key => "list/example_list/message/2006/10/goodid@example.com")
-    message.expects(:slug).at_least_once.returns("example_list")
-    message.expects(:date).times(2).returns( mock(:year => 2006, :month => 10) )
-    Message.expects(:new).returns(message)
-
-    f = Filer.new(0, 0)
-    f.store message(:good)
-    assert_equal 1, f.message_count
-    assert_equal({ 'example_list' => [[2006, 10]] }, f.mailing_lists)
-  end
-
-  def test_store_fails
-    message = mock('message')
-    message.expects(:store).raises(RuntimeError, "something bad happened")
-    Message.expects(:new).returns(message)
-    $archive.expects(:[]=) # the error store
-
-    f = Filer.new(0, 0)
-    f.store message(:good)
-    assert_equal 1, f.message_count
-    assert_equal({}, f.mailing_lists)
-  end
-
-  def test_run_empty
-    # no messages
-    f = Filer.new(0, 0)
-    f.expects(:setup)
-    f.expects(:acquire)
-    f.expects(:queue_threader)
-    f.expects(:teardown)
-    f.sequences.expects(:[]=).with("0/#{Process.pid}", 0)
-    f.run
-  end
-
-  class TestRunFiler < Filer
-    def store message, overwrite ; @sequence += 1 ; end
-  end
-  def test_run
-    f = TestRunFiler.new(0, 0)
-    f.expects(:setup)
-    f.expects(:acquire).yields("message", nil)
-    f.expects(:queue_threader)
-    f.expects(:teardown)
-    f.sequences.expects(:[]=).with("0/#{Process.pid}", 1)
-    f.run
-  end
-
-  def test_queue_threader
-    f = Filer.new(0, 0)
-    f.mailing_lists = { 'example_list' => [[2007, 8], [2007, 9]] }
-    q = mock("thread queue")
-    Queue.expects(:new).with(:thread).returns(q)
-    q.expects(:add).with(:slug => "example_list", :year => 2007, :month => "08")
-    q.expects(:add).with(:slug => "example_list", :year => 2007, :month => "09")
-    f.queue_threader
-  end
-
-  def test_sequence_exhaustion
-    # exception should be raised on any attempt to generate an invalid call number
-    f = Filer.new(0, (2 ** 20 + 1))
-    assert_raises SequenceExhausted do
-      f.call_number
+  context 'filers' do
+    setup do
+      # Many tests would otherwise cause the Filer to print its normal output
+      $stdout.expects(:puts).at_least(0)
     end
 
-    # and also immediately after the last message safely read
-    f = Filer.new(0, (2 ** 20))
-    f.expects(:setup)
-    f.expects(:acquire).yields(:message, nil)
-    f.expects(:store).with(:message, nil).raises(SequenceExhausted, "sequence exhausted")
-    f.sequences.expects(:[]=).with("0/#{Process.pid}", 2 ** 20)
-    assert_raises SequenceExhausted do
+    should 'store messages' do
+      message = mock('message', :store => true, :key => "list/example_list/message/2006/10/goodid@example.com")
+      message.expects(:slug).at_least_once.returns("example_list")
+      message.expects(:date).times(2).returns( mock(:year => 2006, :month => 10) )
+      Message.expects(:new).returns(message)
+
+      f = Filer.new(0, 0)
+      f.store message(:good)
+      assert_equal 1, f.message_count
+      assert_equal({ 'example_list' => [[2006, 10]] }, f.mailing_lists)
+    end
+
+    should 'handle storage failures' do
+      message = mock('message')
+      message.expects(:store).raises(RuntimeError, "something bad happened")
+      Message.expects(:new).returns(message)
+      $archive.expects(:[]=) # the error store
+
+      f = Filer.new(0, 0)
+      f.store message(:good)
+      assert_equal 1, f.message_count
+      assert_equal({}, f.mailing_lists)
+    end
+
+    should 'run cleanly when there are no messages' do
+      # no messages
+      f = Filer.new(0, 0)
+      f.expects(:setup)
+      f.expects(:acquire)
+      f.expects(:queue_threader)
+      f.expects(:teardown)
+      f.sequences.expects(:[]=).with("0/#{Process.pid}", 0)
       f.run
     end
+
+    class TestRunFiler < Filer
+      def store message, overwrite ; @sequence += 1 ; end
+    end
+    should 'run' do
+      f = TestRunFiler.new(0, 0)
+      f.expects(:setup)
+      f.expects(:acquire).yields("message", nil)
+      f.expects(:queue_threader)
+      f.expects(:teardown)
+      f.sequences.expects(:[]=).with("0/#{Process.pid}", 1)
+      f.run
+    end
+
+    should 'queue the threader' do
+      f = Filer.new(0, 0)
+      f.mailing_lists = { 'example_list' => [[2007, 8], [2007, 9]] }
+      q = mock("thread queue")
+      Queue.expects(:new).with(:thread).returns(q)
+      q.expects(:add).with(:slug => "example_list", :year => 2007, :month => "08")
+      q.expects(:add).with(:slug => "example_list", :year => 2007, :month => "09")
+      f.queue_threader
+    end
+
+    should 'handle sequence exhaustion' do
+      # exception should be raised on any attempt to generate an invalid call number
+      f = Filer.new(0, (2 ** 20 + 1))
+      assert_raises SequenceExhausted do
+        f.call_number
+      end
+
+      # and also immediately after the last message safely read
+      f = Filer.new(0, (2 ** 20))
+      f.expects(:setup)
+      f.expects(:acquire).yields(:message, nil)
+      f.expects(:store).with(:message, nil).raises(SequenceExhausted, "sequence exhausted")
+      f.sequences.expects(:[]=).with("0/#{Process.pid}", 2 ** 20)
+      assert_raises SequenceExhausted do
+        f.run
+      end
+    end
+
+    should 'handle store() double failure' do
+      Message.expects(:new).raises(RuntimeError, "Primary Error")
+      $archive.expects(:[]=).raises(RuntimeError, "Secondary Error")
+
+      rc = mock("remote connection")
+      RemoteConnection.expects(:new).returns(rc)
+      rc.expects(:upload_file)
+
+      f = Filer.new(0, 0)
+      f.expects(:release)
+      f.store "mail"
+    end
+
   end
-
-  def test_store_double_failure
-    Message.expects(:new).raises(RuntimeError, "Primary Error")
-    $archive.expects(:[]=).raises(RuntimeError, "Secondary Error")
-
-    rc = mock("remote connection")
-    RemoteConnection.expects(:new).returns(rc)
-    rc.expects(:upload_file)
-
-    f = Filer.new(0, 0)
-    f.expects(:release)
-    f.store "mail"
-  end
-
 end
