cf38f6c058d8f58ea3cd96c842a8f810f85401cd
Peter Harkins <ph@malaprop.org>
2010-04-22 20:47:54 -0500

    Converted Log tests to shoulda.
    BugzId: 1116

--- test/unit/log_test.rb
+++ test/unit/log_test.rb
@@ -1,43 +1,49 @@
 require 'test_helper'
 
 class LoggerHashTest < ActiveSupport::TestCase
-  def setup
-    @log = Log.new 'worker'
-  end
-  def teardown ; @log = nil ; end
-
-  def test_initialize
-    assert_equal 'worker', @log.worker
-  end
-
-  def test_begin
-    @log.expects(:log).with(:begin, 'message')
-    @log.begin('key', 'message')
-    assert_equal 'key', @log.key
-  end
-
-  def test_end
-    @log.expects(:log).with(:begin, 'message')
-    @log.begin('key', 'message')
-
-    @log.expects(:log).with(:end, 'end message')
-    @log.end('end message')
-    assert @log.key.nil?
-  end
-
-  def test_block
-    @log.expects(:begin).with('key', 'message')
-    @log.expects(:end).with('end message')
-    @log.block 'key', 'message' do |log|
-      assert_not_equal log, @log # should get a new instance for sub-jobs
-      "end message"
+  context 'a Log' do
+    setup do
+      @log = Log.new 'worker'
     end
-  end
 
-  def test_messaging
-    [:error, :warning, :status].each do |status|
-      @log.expects(:log).with(status, "message #{status}")
-      @log.send(status, "message #{status}")
+    teardown do
+      @log = nil
     end
+
+    should 'be a worker' do
+      assert_equal 'worker', @log.worker
+    end
+
+    should 'log a begin message' do
+      @log.expects(:log).with(:begin, 'message')
+      @log.begin('key', 'message')
+      assert_equal 'key', @log.key
+    end
+
+    should 'log an end message' do
+      @log.expects(:log).with(:begin, 'message')
+      @log.begin('key', 'message')
+
+      @log.expects(:log).with(:end, 'end message')
+      @log.end('end message')
+      assert @log.key.nil?
+    end
+
+    should 'log in a block' do
+      @log.expects(:begin).with('key', 'message')
+      @log.expects(:end).with('end message')
+      @log.block 'key', 'message' do |log|
+        assert_not_equal log, @log # should get a new instance for sub-jobs
+        "end message"
+      end
+    end
+
+    should 'log different kinds of messages' do
+      [:error, :warning, :status].each do |status|
+        @log.expects(:log).with(status, "message #{status}")
+        @log.send(status, "message #{status}")
+      end
+    end
+
   end
 end
