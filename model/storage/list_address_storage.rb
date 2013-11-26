a3655047617e60e72abb2d178998f07594b9c194
Peter Harkins <ph@push.cx>
2013-11-26 08:09:41 -0600

    ListAddressStorage (no ListAddress model) to look up Lists

---
+++ model/storage/list_address_storage.rb
@@ -0,0 +1,12 @@
+require_relative 'list_storage'
+
+class ListAddressStorage
+  include RiakStorage
+
+  # maybe this is hinky, but no one actually care to hang onto the
+  # intermediate slug and look up the List themselves
+  def self.find_list_by_address address
+    slug = bucket[address]
+    ListStorage.find(slug)
+  end
+end
---
+++ spec/isolation/storage/list_address_storage_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../../rspec'
+require_relative '../../../model/storage/list_address_storage'
+
+describe ListAddressStorage do
+  describe '#find_list_by_address' do
+    it 'finds lists' do
+      # This am stubbed to death. Am I even testing anything?
+      bucket = double('bucket')
+      bucket.should_receive(:[]).with('list@example.com').and_return('slug')
+      ListAddressStorage.should_receive(:bucket).and_return(bucket)
+      ListStorage.should_receive(:find).with('slug')
+      list = ListAddressStorage.find_list_by_address('list@example.com')
+    end
+  end
+end
