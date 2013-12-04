8084d325782214477bd52301b12933f56babfb93
Peter Harkins <ph@push.cx>
2013-12-04 14:47:33 -0600

    Extract MessageId model

---
+++ model/message_id.rb
@@ -0,0 +1,43 @@
+class MessageId
+  attr_reader :raw, :message_id
+
+  def initialize raw
+    @raw = raw || ''
+  end
+
+  def valid?
+    !raw.empty? and raw.length <= 120 and has_id?
+  end
+  
+  def has_id?
+    raw =~ /^<?[a-zA-Z0-9%+\-\.=_]+@[a-zA-Z0-9_\-\.]+>?$/
+  end
+
+  def extract_id
+    @message_id ||= /^<?([^@]+@[^\>]+)>?/.match(raw)[1].chomp
+  end
+
+  def to_s
+    if valid?
+      extract_id
+    else
+      "[invalid or missing message id]"
+    end
+  end
+
+  def inspect
+    "#<MessageId:%x '%s'>" % [(object_id << 1), to_s]
+  end
+
+  def self.generate_for call_number
+    raise ArgumentError, "Missing call number to generate MessageId" if (call_number || '') == ''
+
+    new "#{call_number}@generated-message-id.chibrary.org"
+  end
+
+  def self.extract_or_generate str, call_number
+    mid = new(str)
+    return mid if mid.valid?
+    return generate_for call_number
+  end
+end
---
+++ spec/isolation/model/message_id_spec.rb
@@ -0,0 +1,83 @@
+require_relative '../../rspec'
+require_relative '../../../model/message_id'
+
+describe MessageId do
+  describe '.valid?' do
+    it 'is valid if well-formatted' do
+      expect(MessageId.new('<valid@example.com>')).to be_valid
+    end
+
+    it 'is invalid if nil' do
+      expect(MessageId.new nil).to_not be_valid
+    end
+
+    it 'is invalid if empty string' do
+      expect(MessageId.new('')).to_not be_valid
+    end
+
+    it 'is invalid if longer than 120 characters' do
+      str = ('x' * 109) + "@example.com"
+      expect(MessageId.new str).to_not be_valid
+    end
+
+    it 'is invalid if it does not include an id' do
+      expect(MessageId.new 'I like cats').to_not be_valid
+    end
+  end
+
+  describe '.has_id?' do
+    it 'does with and without angle brackets' do
+      expect(MessageId.new '<with@example.com>').to have_id
+      expect(MessageId.new 'without@example.com').to have_id
+    end
+
+    it 'does not with multiple @s' do
+      expect(MessageId.new 'bad@heart@example.com').to_not have_id
+    end
+
+    it 'does not with no text before or after the @' do
+      expect(MessageId.new 'bad@').to_not have_id
+      expect(MessageId.new '@example.com').to_not have_id
+    end
+  end
+
+  describe '.to_s' do
+    it 'returns the extracted id' do
+      expect(MessageId.new('<id@example.com>').to_s).to eq('id@example.com')
+    end
+    it 'does not pass invalid ids' do
+      s = MessageId.new('cats are great').to_s
+      expect(s).to_not include('cats')
+      expect(s).to include('invalid')
+    end
+  end
+
+  describe '.inspect' do
+    it 'includes the .to_s' do
+      expect(MessageId.new('id@example.com').inspect).to include('id@example.com')
+      expect(MessageId.new('cats rule').inspect).to include('invalid')
+    end
+  end
+
+  describe '#generate_for' do
+    it 'creates based on call number' do
+      expect(MessageId.generate_for('0123456789').to_s).to include('0123456789')
+    end
+    it 'raises without a call_number' do
+      expect {
+        MessageId.generate_for ''
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#extract_or_generate' do
+    it 'given a valid message id, creates from that' do
+      mid = MessageId.extract_or_generate('id@example.com', 'call')
+      expect(mid.to_s).to include('id@example.com')
+    end
+    it 'given an invalid message id, generates from call_number' do
+      mid = MessageId.extract_or_generate('srsly cats man', 'call')
+      expect(mid.to_s).to include('call')
+    end
+  end
+end
