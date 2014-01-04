bfa639735b8b25ce13e9253e43627513d5100aa6
Peter Harkins <ph@push.cx>
2014-01-04 09:31:28 -0600

    SummaryStorage

---
+++ model/storage/summary_storage.rb
@@ -0,0 +1,26 @@
+require 'time'
+
+require_relative '../summary'
+
+# Does not import RiakStorage completely confirm to the Storage interface
+# because Summaries are never loaded individually, only via
+# SummaryContainerStorage.
+class SummaryStorage
+  attr_reader :summary
+
+  def initialize summary
+    @summary = summary
+  end
+
+  def to_hash
+    {
+      call_number: summary.call_number.to_s,
+      n_subject:   summary.n_subject,
+      date:        summary.date.rfc2822,
+    }
+  end
+
+  def from_hash h
+    Summary.new h[:call_number], h[:n_subject], h[:date]
+  end
+end
---
+++ spec/isolation/storage/summary_storage_spec.rb
@@ -0,0 +1,18 @@
+require 'time'
+
+require_relative '../../rspec'
+require_relative '../../../model/storage/summary_storage'
+
+describe SummaryStorage do
+  context 'instantiated with a Message' do
+    describe '#to_hash' do
+      let(:now) { Time.now }
+      let(:summary) { OpenStruct.new(call_number: 'callnumber', n_subject: 'foo', date: now) }
+      subject { SummaryStorage.new(summary).to_hash }
+
+      it { expect(subject[:call_number]).to eq('callnumber') }
+      it { expect(subject[:n_subject]).to eq('foo') }
+      it { expect(subject[:date]).to eq(now.rfc2822) }
+    end
+  end
+end
