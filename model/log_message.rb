7c578809955b8861ed282d3921f71a0cfde4a98b
Peter Harkins <ph@push.cx>
2014-02-06 20:44:22 -0600

    Remove old storage primitives

--- model/log_message.rb
+++
@@ -1,6 +0,0 @@
-class LogMessage #< ActiveRecord::Base
-  attr_accessor :server, :pid, :worker, :key, :status, :message
-
-  #validates_presence_of :server, :pid, :worker, :key, :status, :message
-  #validates_numericality_of :server, :pid
-end
