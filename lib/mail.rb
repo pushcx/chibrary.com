af5886b7838b02727c8a8d6d64eef55b8c7dbab9
Peter Harkins <ph@push.cx>
2014-05-04 22:57:18 -0500

    Old config file messing with Mail gem

--- lib/mail.rb
+++
@@ -1,6 +0,0 @@
-MAIL_SERVER    = 'mail.chibrary.com'
-MAIL_ARCHIVE   = 'archive@chibrary.com'
-MAIL_POP3_PORT = 110
-MAIL_SMTP_PORT = 587
-MAIL_USER      = 'archive@chibrary.com'
-MAIL_PASSWORD  = 'y7fX$e2Z'
