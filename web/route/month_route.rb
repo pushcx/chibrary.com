a502d0b19b3e47a9a87ea1c434d3961507ff5003
Peter Harkins <ph@push.cx>
2014-05-11 17:00:56 -0500

    Looking up n/p month links and ThreadSets

--- web/route/month_route.rb
+++ web/route/month_route.rb
@@ -1,14 +1,13 @@
-require_relative '../../value/sym'
-
 get '/:slug/:year/:month/?' do
   load_list
   load_month
+  sym = Sym.new(@slug, @year, @month)
 
   @title = "#{@list.title_name} #{@year}-#{@month} archive"
-  @month_count = MonthCountRepo.find Sym.new(@slug, @year, @month)
+  @month_count = MonthCountRepo.find sym
   raise Sinatra::NotFound, "No messages in #{@slug}/#{@year}/#{@month}" if @month_count.empty?
-  @previous_link, @next_link = month_previous_next(Sym.new(@slug, @year, @month))
-  @threadset = ThreadSet.month(@slug, @year, @month)
+  @previous_link, @next_link = month_previous_next sym
+  @threadset = ThreadSetRepo.month sym
 
   haml :'month/show.html'
 end
