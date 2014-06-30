3be58d4dd7e6041d7d6c555beeae737dab4a497e
Peter Harkins <ph@push.cx>
2014-06-30 16:54:18 -0500

    Remove unused imports

--- script/threader.rb
+++ script/threader.rb
@@ -2,12 +2,6 @@
 
 require_relative '../worker/thread_worker'
 require_relative '../value/sym'
-require_relative '../model/message'
-require_relative '../model/thread_set'
-require_relative '../repo/call_number_list_repo'
-require_relative '../repo/message_repo'
-require_relative '../repo/thread_set_repo'
-require_relative '../lib/core_ext/time_'
 
 begin
   ThreadWorker.new.thread(Sym.new('mud-dev', 2000, 1)) if __FILE__ == $0
