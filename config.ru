d9cd947d1130edabbf239ede00086f6bdf6a5ff4
Peter Harkins <ph@push.cx>
2014-10-06 12:10:09 -0500

    Move web setup into config.ru so .pry can use env for easy tinkering

--- config.ru
+++ config.ru
@@ -1,6 +1,29 @@
 # Require config/environment.rb
 require ::File.expand_path('../config/environment',  __FILE__)
 
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/partial'
+
+# these specific routes need to get loaded first so that list doesn't shadow them
+require 'web/route/about_route'
+require 'web/route/search_route'
+# and these need to be loaded next so that month view doesn't shadow them
+require 'web/route/thread_route'
+require 'web/route/message_route'
+Dir['web/route/**/*.rb', 'web/helper/**/*.rb'].each { |f| require f }
+
+before do
+  @title = "Chibrary - Free Mailing List Archives"
+end
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+
 set :app_file, __FILE__
 set :haml, format: :html5, layout: :'layout/application.html'
 
--- config/environment.rb
+++ config/environment.rb
@@ -6,36 +6,6 @@ $LOAD_PATH << File.expand_path('../../', __FILE__)
 require 'rubygems'
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-require 'sinatra'
-require 'sinatra/reloader' if development?
-require 'sinatra/partial'
-
-require 'logger'
-require 'pathname'
-
 # Some helper constants for path-centric logic
-APP_ROOT = Pathname.new(__FILE__)
-APP_NAME = 'chibrary'
-
-Dir["lib/**/*.rb", 'value/**/*.rb', 'model/**/*.rb', 'repo/**/*.rb'].each { |f| require f }
+Dir["lib/**/*.rb", 'value/**/*.rb', 'model/**/*.rb', 'repo/**/*.rb', 'worker/**/*.rb'].each { |f| require f }
 include Chibrary
-
-# finally, bring up the web stack
-# these specific routes need to get loaded first so that list doesn't shadow them
-require 'web/route/about_route'
-require 'web/route/search_route'
-# and these need to be loaded next so that month view doesn't shadow them
-require 'web/route/thread_route'
-require 'web/route/message_route'
-Dir['web/route/**/*.rb', 'web/helper/**/*.rb'].each { |f| require f }
-
-before do
-  @title = "Chibrary - Free Mailing List Archives"
-end
-
-helpers do
-  def h(text)
-    Rack::Utils.escape_html(text)
-  end
-end
-
