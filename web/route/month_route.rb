23ec6888d5021efe75879ca50649766463c8b1fd
Peter Harkins <ph@push.cx>
2014-10-07 21:16:16 -0500

    typo fix

--- web/route/month_route.rb
+++ web/route/month_route.rb
@@ -2,7 +2,7 @@ get '/:slug/:year/:month/?' do
   load_list
   load_month
 
-  @title = "#{@list.title_name} #{@sym.year}-#{@sym.month} archive"
+  @title = "#{@list.title_name} #{@sym.when} archive"
   @month_count = MonthCountRepo.find @sym
   raise Sinatra::NotFound, "No messages in #{@sym.to_key}" if @month_count.empty?
   @previous_link, @next_link = month_previous_next @sym
