8ba004666e116ffab3a4c74cefb9a2d603a84fed
Peter Harkins <ph@push.cx>
2014-10-20 11:59:46 -0500

    Heuristic tweak: match substantial quotes when joining threads to avoid joining on list footer, etc

--- model/thread.rb
+++ model/thread.rb
@@ -53,7 +53,7 @@ class Thread
     # Warning: doesn't check lists, ThreadRepo.thread_for does
     return true if message_ids.include? message.message_id
     return true if n_subjects.include? message.n_subject
-    return containers.values.any? { |c| c.message.lines_matching(message.direct_quotes) > 0 }
+    return containers.values.any? { |c| c.message.lines_matching(message.direct_quotes) > 3 }
   end
 
   def << message_or_container
